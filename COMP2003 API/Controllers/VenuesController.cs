398336b48b4cd9b1124f7adcb3b59bb927ea1ef7
Lewis Trowbridge <lewbob9927@gmail.com>
2021-02-23 13:56:28 +0000

    Removed template controllers and added skeleton venues controller

---
+++ COMP2003 API/Controllers/VenuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using COMP2003_API.Models;
+using COMP2003_API.Responses;
+
+namespace COMP2003_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VenuesController : ControllerBase
+    {
+        private readonly cleanTableDbContext _context;
+
+        [HttpGet]
+        public ActionResult<List<VenuesSearchResult>> Search(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsPostcode(string searchString)
+        {
+            return false;
+        }
+    }
+}
