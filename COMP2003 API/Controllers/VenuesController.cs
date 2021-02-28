9b6ad68c4476fefafbff461ccdac8ba9751ec489
Nathan <nathan.everett@students.plymouth.ac.uk>
2021-02-28 02:53:02 +0000

    if statement for testing postcode, searchstring formatted (in comparison not actual, so that it returns as it was input), and regex added
    also comments, and function skeleton

--- COMP2003 API/Controllers/VenuesController.cs
+++ COMP2003 API/Controllers/VenuesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using COMP2003_API.Models;
 using COMP2003_API.Responses;
+using System.Text.RegularExpressions;
 
 namespace COMP2003_API.Controllers
 {
@@ -24,8 +25,10 @@ namespace COMP2003_API.Controllers
         [HttpGet("search")]
         public ActionResult<List<VenuesSearchResult>> Search(string searchString)
         {
-            if (IsPostcode(searchString))
+            //Regex rx = new Regex("(?:(?<a>[Gg][Ii][Rr])(?<d>)(?<s>0)(?<u>[Aa]{2}))|(?:(?:(?:(?<a>[A-Za-z])(?<d>[0-9]{1,2}))|(?:(?:(?<a>[A-Za-z][A-Ha-hJ-Yj-y])(?<d>[0-9]{1,2}))|(?:(?:(?<a>[A-Za-z])(?<d>[0-9][A-Za-z]))|(?:(?<a>[A-Za-z][A-Ha-hJ-Yj-y])(?<d>[0-9]?[A-Za-z])))))(?<s>[0-9])(?<u>[A-Za-z]{2}))", RegexOptions.None | RegexOptions.Compiled);
+            if (IsPostcode(searchString)) 
             {
+                //Loop through, rate postcode, put in list.
                 throw new NotImplementedException();
             }
             else
@@ -50,9 +53,17 @@ namespace COMP2003_API.Controllers
             }
         }
 
+        private int RatingPoscode(string postcodeInputUser, string postcodeInputCompare)
+        {
+            //return rating based on the postcode input by the user and in stored restaurants
+            //using ToUpper() and Replace(" ", String.Empty) so Regex can read it
+            return 0;
+        }
         private bool IsPostcode(string searchString)
         {
-            return false;
+            Regex rx = new Regex(@"^([A-Z][A-HJ-Y]?\d[A-Zz\d]??\d[A-Z]{2}|GIR ?0A{2})$", RegexOptions.IgnoreCase | RegexOptions.Compiled); //^ $ start, end of text to prevent postcode in a sentence, might be worth changing later?
+            Match match = rx.Match(searchString.Replace(" ", String.Empty)); //Replace " " with blank so that regex can read it, might be worth changing the regex later
+            return match.Success;
         }
     }
 }
