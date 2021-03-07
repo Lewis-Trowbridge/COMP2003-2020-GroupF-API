47dde16d2a17daa69cda07d2c05994a252c887e7
Lewis Trowbridge <lewbob9927@gmail.com>
2021-03-07 13:17:14 +0000

    View method returns booking information based on hardcoded customer ID

--- COMP2003 API/Controllers/BookingsController.cs
+++ COMP2003 API/Controllers/BookingsController.cs
@@ -20,10 +20,31 @@ namespace COMP2003_API.Controllers
             _context = context;
         }
 
-        [HttpGet]
-        public Task<ActionResult<AppBookingsView>> View(int bookingId)
+        [HttpGet("view")]
+        public async Task<ActionResult<AppBookingsView>> View(int bookingId)
         {
-            throw new NotImplementedException();
+            // This is where the access of the customer ID would go
+            // which would likely use HttpContext to obtain the customer ID - it is hardcoded for now
+            int customerId = 1;
+            try
+            {
+                // Gets a booking view where passed in booking ID and determined customer ID
+                // match the booking belonging to this customer
+                AppBookingsView booking = await _context.AppBookingsView.Where(
+                booking => booking.BookingId.Equals(bookingId)
+                && booking.CustomerId.Equals(customerId)).FirstAsync();
+                // Pass back view information - no response object is needed since this view contains
+                // all of the information needed across all tables
+                return Ok(booking);
+            }
+
+            // If no booking is found, then this exception will be thrown
+            catch (InvalidOperationException)
+            {
+                // Since the request was correctly formatted, 204 no content is the most fitting response
+                // when no booking is found
+                return NoContent();
+            }
         }
     }
 }
