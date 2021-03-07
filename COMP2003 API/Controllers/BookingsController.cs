d19e166941f4bc6e729ae4294a50180e4c0031e0
Lewis Trowbridge <lewbob9927@gmail.com>
2021-03-07 11:23:27 +0000

    Added bookings controller and skeleton view method

---
+++ COMP2003 API/Controllers/BookingsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using COMP2003_API.Models;
+
+namespace COMP2003_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly cleanTableDbContext _context;
+
+        public BookingsController(cleanTableDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public Task<ActionResult<AppBookingsView>> View(int bookingId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
