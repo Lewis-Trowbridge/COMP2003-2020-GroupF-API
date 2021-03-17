00064709f9d4bd25e6f7658bc2d8743b586a1798
Lewis Trowbridge <lewbob9927@gmail.com>
2021-03-17 20:47:54 +0000

    Updated Entity Framework packages to 3.1.13 and generated skeleton CustomersController

---
+++ COMP2003 API/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using COMP2003_API.Models;
+
+namespace COMP2003_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly COMP2003_FContext _context;
+
+        public CustomersController(COMP2003_FContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
