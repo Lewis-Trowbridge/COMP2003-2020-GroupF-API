841180a51f31cffc09feb99f07a6b31597b6dc56
Lewis Trowbridge <lewbob9927@gmail.com>
2021-03-23 17:10:01 +0000

    Added CreateBooking request model, and refactored BookTable method to use it

---
+++ COMP2003 API/Requests/CreateBooking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace COMP2003_API.Requests
+{
+    public class CreateBooking
+    {
+        [Required]
+        public int VenueTableId { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+        [Required]
+        public DateTime BookingDateTime { get; set; }
+        [Required]
+        public int BookingSize { get; set; }
+    }
+}
