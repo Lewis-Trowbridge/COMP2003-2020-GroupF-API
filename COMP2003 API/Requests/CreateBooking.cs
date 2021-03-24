3e0488cf06e44e9615b27b1ac582623926ce2b1e
Lewis Trowbridge <lewbob9927@gmail.com>
2021-03-24 22:10:11 +0000

    Removed unecessary lines from previous commits

--- COMP2003 API/Requests/CreateBooking.cs
+++ COMP2003 API/Requests/CreateBooking.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
-
-#nullable enable
 
 namespace COMP2003_API.Requests
 {
