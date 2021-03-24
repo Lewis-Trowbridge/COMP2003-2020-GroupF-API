bc3236f65a9fbffa5a6981b7094e23d22f85142f
Lewis Trowbridge <lewbob9927@gmail.com>
2021-03-24 22:37:05 +0000

    Added test for view top to ensure list is returned

---
+++ COMP2003 API.tests/Controllers/VenuesControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using COMP2003_API.Controllers;
+using COMP2003_API.Models;
+using COMP2003_API.Tests.Helpers;
+
+namespace COMP2003_API.Tests.Controllers
+{
+    public class VenuesControllerTests
+    {
+        [Fact]
+        public async void ViewTop_Returns_ListOfAppVenueViews()
+        {
+            // Arrange
+            COMP2003_FContext dbContext = COMP2003TestHelper.GetDbContext();
+            VenuesController controller = new VenuesController(dbContext);
+
+            // Act
+            ActionResult<List<AppVenueView>> actionResult = await controller.ViewTop();
+            var okObjectResult = actionResult.Result as OkObjectResult;
+            var result = (List<AppVenueView>)okObjectResult.Value;
+
+            // Assert
+            Assert.IsType<List<AppVenueView>>(result);
+
+        }
+
+    }
+}
