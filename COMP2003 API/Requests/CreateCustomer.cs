c24e8cd78e73bc48f8f3d6ae48aa656ebb6b39ca
Lewis Trowbridge <lewbob9927@gmail.com>
2021-04-11 11:59:26 +0100

    Added CreateCustomer model and updated validation properties as well as usages

---
+++ COMP2003 API/Requests/CreateCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace COMP2003_API.Requests
+{
+    public class CreateCustomer
+    {
+        [JsonRequired]
+        public string CustomerName { get; set; }
+        [JsonRequired]
+        public string CustomerContactNumber { get; set; }
+        [JsonRequired]
+        public string CustomerUsername { get; set; }
+        [JsonRequired]
+        public string CustomerPassword { get; set; }
+    }
+}
