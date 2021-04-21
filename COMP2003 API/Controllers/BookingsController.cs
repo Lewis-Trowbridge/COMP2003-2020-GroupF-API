c3497fab530401f84f4854ee09f231fddb04d045
Nathan <nathan.everett@students.plymouth.ac.uk>
2021-04-21 18:54:47 +0100

    edit booking API working

--- COMP2003 API/Controllers/BookingsController.cs
+++ COMP2003 API/Controllers/BookingsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Data.SqlClient;
 using COMP2003_API.Models;
 using COMP2003_API.Responses;
+using COMP2003_API.Requests;
 
 namespace COMP2003_API.Controllers
 {
@@ -85,6 +86,64 @@ namespace COMP2003_API.Controllers
             await _context.Database.ExecuteSqlRawAsync("EXEC delete_booking @booking_id, @response OUTPUT", parameters);
 
             return (string)parameters[1].Value;
+        }
+
+        [HttpPut("edit")]
+        public async Task<ActionResult<DeletionResult>> Edit(EditBooking booking)
+        {
+            EditResult result = new EditResult();
+            if (!ModelState.IsValid)
+            {
+                result.Success = false;
+                result.Message = "A validation error occured.";
+                return BadRequest(result);
+            }
+
+            if (!booking.BookingTime.HasValue)
+            {
+                booking.BookingTime = new DateTime(1753, 1, 1, 12, 0, 0);
+            }
+
+            int response = await CallEditBookingSP(booking.BookingId, booking.BookingTime.Value, booking.BookingSize, booking.VenueTableId);
+            switch (response)
+            {
+                case 200:
+                    result.Success = true;
+                    result.Message = "Customer details edited.";
+                    return Ok(result);
+                case 404:
+                    result.Success = false;
+                    result.Message = "Customer not found.";
+                    return StatusCode(404, result);
+                case 400:
+                    result.Success = false;
+                    result.Message = "Booking already at that time.";
+                    return StatusCode(404, result);
+
+                default:
+                    result.Success = false;
+                    result.Message = "An unspecified server error has occured.";
+                    return StatusCode(500, result);
+            }
+
+        }
+        private async Task<int> CallEditBookingSP(int bookingId, DateTime bookingTime, int bookingSize, int venueTableId)
+        {
+            SqlParameter[] parameters = new SqlParameter[5];
+            parameters[0] = new SqlParameter("@booking_id", bookingId);
+            parameters[1] = new SqlParameter("@booking_time", bookingTime);
+            parameters[2] = new SqlParameter("@booking_size", bookingSize);
+            parameters[3] = new SqlParameter("@venue_table_id", venueTableId);
+            parameters[4] = new SqlParameter("@status_code", 0);
+
+            parameters[4].Direction = System.Data.ParameterDirection.Output;
+
+            await _context.Database.ExecuteSqlRawAsync("EXEC edit_booking @booking_id, @booking_time, @booking_size, @venue_table_id, @status_code OUTPUT", parameters);
+
+            System.Diagnostics.Debug.WriteLine("--------------------------------^^" );
+            System.Diagnostics.Debug.WriteLine("--------------------------------^^" );
+            System.Diagnostics.Debug.WriteLine("--------------------------------^^" + parameters[4].Value.ToString());
+            return Convert.ToInt32(parameters[4].Value);
         }
     }
 }
