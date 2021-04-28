73b0cd73158cfdcf72e0bd45370b48e24cff4694
Lewis Trowbridge <lewbob9927@gmail.com>
2021-04-28 19:50:27 +0100

    Added login controller with stub method and LoginResult response struct

---
+++ COMP2003 API/Responses/LoginResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COMP2003_API.Responses
+{
+    public struct LoginResult
+    {
+        public bool Success { get; set; }
+        public int Id { get; set; }
+        public string Message { get; set; }
+    }
+}
