0a5c2e520240d5d5b00129483e5274e98e83d2e1
Lewis Trowbridge <lewbob9927@gmail.com>
2021-04-28 19:50:27 +0100

    Added login controller with stub method and LoginResult response struct

---
+++ COMP2003 API/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using COMP2003_API.Models;
+using COMP2003_API.Responses;
+
+namespace COMP2003_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+
+        private readonly COMP2003_FContext _context;
+
+        public LoginController(COMP2003_FContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<LoginResult> Login(string customerUsername, string customerPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
